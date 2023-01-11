9c89af7ecda21499a50788c51a80d6c9dd37104a
Reza Shojaee <reza.shj@gmail.com>
2023-01-11 11:51:35 +0330

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace GA_Subset_sum
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Browse_b = new System.Windows.Forms.Button();
+            this.Output_t = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.Generation_t = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Browse_b
+            // 
+            this.Browse_b.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Browse_b.ForeColor = System.Drawing.Color.Navy;
+            this.Browse_b.Location = new System.Drawing.Point(12, 17);
+            this.Browse_b.Name = "Browse_b";
+            this.Browse_b.Size = new System.Drawing.Size(436, 57);
+            this.Browse_b.TabIndex = 0;
+            this.Browse_b.Text = "Browse for input file...";
+            this.Browse_b.UseVisualStyleBackColor = true;
+            this.Browse_b.Click += new System.EventHandler(this.Browse_b_Click);
+            // 
+            // Output_t
+            // 
+            this.Output_t.Location = new System.Drawing.Point(12, 187);
+            this.Output_t.Name = "Output_t";
+            this.Output_t.ReadOnly = true;
+            this.Output_t.Size = new System.Drawing.Size(438, 162);
+            this.Output_t.TabIndex = 2;
+            this.Output_t.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(9, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Output";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Generation_t);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.groupBox1.Location = new System.Drawing.Point(91, 81);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(357, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(9, 23);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(130, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Use infinite loop";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(9, 58);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(249, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Maximum number of generations =";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Generation_t
+            // 
+            this.Generation_t.Location = new System.Drawing.Point(264, 56);
+            this.Generation_t.Name = "Generation_t";
+            this.Generation_t.Size = new System.Drawing.Size(87, 21);
+            this.Generation_t.TabIndex = 2;
+            this.Generation_t.Text = "1000000";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(457, 363);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Output_t);
+            this.Controls.Add(this.Browse_b);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Solving subset sum problem with genetic algorithm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Browse_b;
+        private System.Windows.Forms.RichTextBox Output_t;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox Generation_t;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GA_Subset_sum
+{
+    public partial class MainForm : Form
+    {
+        int N;       // Number of members
+        int Goal;    // Goal number
+        int[] myset;
+        int population_size = 40;
+        int[] chromosome;        // array for chromosomes which represent choice variable   0 or 1
+        int[,] population;
+        int[,] Offsprings;
+        double[] fitness_value;
+        double[] OffspringsFitnessValue;
+        int NegativeTotal = 0;
+        int PositiveTotal = 0;
+        int counter = 0;
+        string outpput = "";
+        Int64 generation;
+        double MutationRate = 0.15;
+        int TournamentSize = 10;
+        int[] tour;
+        
+        
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+       
+        void CreateInitialPopulation()
+        {
+            Random rnd = new Random();
+            int tt = 0;
+            for (int i = 0; i < population_size; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    tt = rnd.Next(1, 10000);
+                    if (tt < 5000)
+                        population[i,j] = 0;
+                    else
+                        population[i,j] = 1;
+                }
+            }
+        }
+
+        double CalculateFitness(int[,] x, int index)
+        {
+            int sum = 0;
+            for (int i = 0; i < N; i++)
+            {
+                sum += myset[i] * x[index,i];
+            }
+
+            int MaxDistance = 0;
+
+            MaxDistance = Math.Max(PositiveTotal - Goal, Goal - NegativeTotal);
+            MaxDistance = Math.Max(MaxDistance, Goal);
+
+            double t1 = Math.Abs(sum - Goal);
+            double t2 = (double)(t1 / MaxDistance);
+            double t3 = Math.Sqrt(t2);
+            double t4 = 1 - t3;
+            return t4;
+        }
+
+        bool EvaluatePopulation()
+        {
+            for (int i = 0; i < population_size; i++)
+            {
+                fitness_value[i] = CalculateFitness(population, i);
+                if (fitness_value[i] == 1)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        if (population[i, j] == 1)
+                        {
+                            counter++;
+                            outpput += myset[j].ToString() + " ";
+                        }
+                    }
+                    if (counter > 0)
+                    {
+                        outpput = counter.ToString() + "\n" + outpput.TrimEnd();
+                        Output_t.Text = outpput;
+                        return true;
+                    }
+                }
+            }
+
+            counter = 0;
+            outpput = "";
+            return false;
+        }
+
+        int Find_Max_Fitness(int[] x)
+        {
+            int Max = x[0];
+
+            for (int i = 1; i < 10; i++)
+            {
+                if(fitness_value[x[i]] > fitness_value[Max])
+                    Max = x[i];
+            }
+            return Max;
+        }
+
+        void TournamentSelection()
+        {
+            int[] k = new int[10];
+            Random myrnd = new Random();
+
+            for (int j = 0; j < TournamentSize; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    k[i] = myrnd.Next(0, population_size);
+                }
+                tour[j] = Find_Max_Fitness(k);
+            }
+        }
+
+        void CrossOver()
+        {
+            int Start = 1;
+            int End = 0;
+            int j = 0;
+            Random myrnd = new Random();
+
+            while (Start >= End)
+            {
+                Start = myrnd.Next(0, N);
+                End = myrnd.Next(0, N);
+            }
+            for (int i = 0; i < TournamentSize - 1; i += 2)
+            {
+                for (j = 0; j < Start; j++)
+                {
+                    Offsprings[i, j] = population[tour[i], j];
+                    Offsprings[i + 1, j] = population[tour[i + 1], j];
+                }
+                for (j = Start; j < End; j++)
+                {
+                    Offsprings[i, j] = population[tour[i + 1], j];
+                    Offsprings[i + 1, j] = population[tour[i], j];
+                }
+                for (j = End; j < N; j++)
+                {
+                    Offsprings[i, j] = population[tour[i], j];
+                    Offsprings[i + 1, j] = population[tour[i + 1], j];
+                }
+            }
+        }
+
+        void Mutate()
+        {
+            Random myrnd = new Random();
+            for (int i = 0; i < TournamentSize; i++)
+                for (int j = 0; j < N; j++)
+                {
+                    if (myrnd.NextDouble() < MutationRate)
+                    {
+                        if (Offsprings[i, j] == 0)
+                            Offsprings[i, j] = 1;
+                        else
+                            Offsprings[i, j] = 0;
+                    }
+                }
+        }
+
+        bool EvaluateOffsprings()
+        {
+            for (int i = 0; i < TournamentSize; i++)
+            {
+                OffspringsFitnessValue[i] = CalculateFitness(Offsprings, i);
+                if (OffspringsFitnessValue[i] == 1)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        if (Offsprings[i, j] == 1)
+                        {
+                            counter++;
+                            outpput += myset[j].ToString() + " ";
+                        }
+                    }
+                    if (counter > 0)
+                    {
+                        outpput = counter.ToString() + "\n" + outpput.TrimEnd();
+                        Output_t.Text = outpput;
+                        return true;
+                    }
+                }
+            }
+            counter = 0;
+            outpput = "";
+            return false;
+        }
+
+        void CreateNextGeneration()
+        {
+            int Max = 0;
+            for (int i = 1; i < TournamentSize; i++)
+                if (OffspringsFitnessValue[i] > OffspringsFitnessValue[Max])
+                    Max = i;
+
+            int Min = 0;
+            for (int j = 1; j < population_size; j++)
+                if (fitness_value[j] < fitness_value[Min])
+                    Min = j;
+
+            for (int k = 0; k < N; k++)
+                population[Min, k] = Offsprings[Max, k];
+
+        }
+
+        void runGA()
+        {
+            CreateInitialPopulation();
+
+            if (EvaluatePopulation() == true)
+                return;
+
+            if(radioButton1.Checked == true)
+                while (true)
+                {
+                    TournamentSelection();
+                    CrossOver();
+                    Mutate();
+                    if (EvaluateOffsprings() == true)
+                        return;
+                    CreateNextGeneration();
+                }
+
+            if(radioButton2.Checked == true)
+            {
+                generation = Int64.Parse(Generation_t.Text);
+                while (generation > 0)
+                {
+                    TournamentSelection();
+                    CrossOver();
+                    Mutate();
+                    if (EvaluateOffsprings() == true)
+                        return;
+                    CreateNextGeneration();
+                    generation--;
+                }
+            }
+
+
+        }
+
+        private void Browse_b_Click(object sender, EventArgs e)
+        {
+            outpput = "";
+            counter = 0;
+            Output_t.Clear();
+            fitness_value = new double[population_size];
+            OffspringsFitnessValue = new double[TournamentSize];
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Browse for input file ...";
+            dlg.Filter = "Text Documents(*.txt)|*.txt";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                String[] input = File.ReadAllLines(dlg.FileName);
+                N = int.Parse(input[0].Substring(0, input[0].IndexOf(' ')));
+                population = new int[population_size, N];
+                Offsprings = new int[TournamentSize, N];
+                tour = new int[10];
+                myset = new int[N];
+                chromosome = new int[N];
+                Goal = int.Parse(input[0].Substring(input[0].IndexOf(' ') + 1, input[0].Length - input[0].IndexOf(' ') - 1));
+                string temp = input[1];
+                int i;
+                for (i = 0; i < N - 1; i++)
+                {
+                    myset[i] = int.Parse(temp.Substring(0, temp.IndexOf(' ')));
+                    temp = temp.Substring(temp.IndexOf(' ') + 1, temp.Length - temp.IndexOf(' ') - 1);
+                }
+                myset[i] = int.Parse(temp);
+
+                for (i = 0; i < N; i++)
+                {
+                    if (myset[i] < 0)
+                        NegativeTotal += myset[i];
+                    else
+                        PositiveTotal += myset[i];
+                }
+
+                runGA();
+            }
+        }
+    }
+}
